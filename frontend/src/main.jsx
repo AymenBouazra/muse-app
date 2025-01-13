df5704a1cfeb7d873c371de56baa37444b8e5314
Aymen <aymenbouazra994@gmail.com>
2025-01-13 01:08:14 +0100

    fix(frontend): remove old import from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { UserProvider } from './utils/context/UserContext.jsx'
 import { store } from './store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <App />
     </Provider>
   </StrictMode>,
 )
